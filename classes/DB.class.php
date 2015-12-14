75f3353c42e9c7b950e059c416f809a9a605ada5
Tobias Landén Hessulf <tops@Tobiass-MacBook.local>
2015-12-14 10:12:14 +0100

    Moved the information for signing in to the database to a config-file instead of having it right in the DB class.

--- classes/DB.class.php
+++ classes/DB.class.php
@@ -18,7 +18,8 @@ class DB{
 	public static function getInstance(){
 		if(!self::$instance){ # Om vi redan har något i $instance i klassen (self::)
 			# Skapa då ett mysqli-objekt med en kopplaing till vår databas och lagra den i $instance
-			self::$instance = new mysqli("localhost","root","","phlogger");
+			require_once('db.conf.php');
+			self::$instance = new mysqli(CONF_DB_SERVER,CONF_DB_USERNAME,CONF_DB_PASSWORD,CONF_DB_DATABASE);
 			return self::$instance;
 		}else{ # Om vi inte har något i $instance
 			return self::$instance;
